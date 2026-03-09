3ae1d99e8a94dc841b04ba522efefbbe2ab62ce9
U-OLUWASUNMISOLA\user <pakinsande@gmail.com>
2026-03-09 17:01:18 -0400

    fix: broadened migration path filters

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
-const db = require('./configs/db'); // Import the db connection
+const db = require('./configs/db'); // Import the db connection very important
 
 const app = express();
 
